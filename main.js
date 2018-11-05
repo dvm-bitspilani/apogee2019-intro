0114a6236b47fcffe70018369d6a91d2a0331f9a
Sarthak Sehgal <sarthaksehgal00@gmail.com>
2018-11-05 22:10:34 +0530

    Initialise particles js

---
+++ main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    particlesJS.load('particles-js', 'particlesjs-config.json', function () {
+        console.log('callback - particles.js config loaded');
+    });
+});
