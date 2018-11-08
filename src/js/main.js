7e79ad3b7648b9ea06c2d9f0b5ca0e2cac7fd409
Sarthak Sehgal <sarthaksehgal00@gmail.com>
2018-11-08 20:44:23 +0530

    change particle color on scroll

--- src/js/main.js
+++ src/js/main.js
@@ -14,12 +14,14 @@ document.addEventListener("DOMContentLoaded", function () {
         {
             id: "about-image-container",
             background: "#0A81D1",
-            fontColor: "#fff"
+            fontColor: "#fff",
+            name: "about"
         },
         {
             id: "paperproject-image-container",
             background: "#faeb4c",
-            fontColor: "#000B37"
+            fontColor: "#000B37",
+            name: "paperproject"
         }
     ];
 
@@ -30,24 +32,49 @@ document.addEventListener("DOMContentLoaded", function () {
         if (scrollTimer) { clearTimeout(scrollTimer); }
         scrollTimer = setTimeout(function () {
             scrollHandler();
-        }, 100);
+        }, 0);
     });
-    
-    function scrollHandler () {
+
+    let currDiv = null;
+
+    function scrollHandler() {
         // change background
         divs.map(div => {
-            if(document.getElementById(div.id).getBoundingClientRect().top >= 0 && document.getElementById(div.id).getBoundingClientRect().top/viewportHeight < 0.7) {
+            if (document.getElementById(div.id).getBoundingClientRect().top >= 0 && document.getElementById(div.id).getBoundingClientRect().top / viewportHeight < 0.7) {
                 document.getElementById("main-image-container").style.backgroundColor = div.background;
                 // change font color for navbar and footer
                 document.getElementById("navbar").style.color = div.fontColor;
                 document.getElementById("footer").style.color = div.fontColor;
+                if (!currDiv)
+                    currDiv = div.name;
+                let particles = document.getElementsByClassName("particles-js");
+                for (let i = 0; i < particles.length; i++) {
+                    if (particles[i].id.split('-')[2] !== div.name) {
+                        particles[i].style.opacity = 0;
+                    } else {
+                        particles[i].style.opacity = 1;
+                    }
+                }
             }
-            if(document.getElementById(div.id).getBoundingClientRect().top <=0 && Math.abs(document.getElementById(div.id).getBoundingClientRect().bottom/viewportHeight) > 0.7) {
+            if (document.getElementById(div.id).getBoundingClientRect().top <= 0 && Math.abs(document.getElementById(div.id).getBoundingClientRect().bottom / viewportHeight) > 0.7) {
                 document.getElementById("main-image-container").style.backgroundColor = div.background;
                 document.getElementById("navbar").style.color = div.fontColor;
                 document.getElementById("footer").style.color = div.fontColor;
+                if (!currDiv)
+                    currDiv = div.name;
+                let particles = document.getElementsByClassName("particles-js");
+                for (let i = 0; i < particles.length; i++) {
+                    if (particles[i].id.split('-')[2] !== div.name) {
+                        particles[i].style.opacity = 0;
+                    } else {
+                        particles[i].style.opacity = 1;
+                    }
+                }
+                // document.getElementById("particles-js-"+currDiv).style.opacity = 0;
+                // document.getElementById("particles-js-"+div.name).style.opacity = 1;
             }
         })
-
     }
+
+    scrollHandler();
 });
