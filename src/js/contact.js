189385555a21619175751628a13894dc55519250
kunal-mohta <kunalmohta1818@gmail.com>
2018-11-18 00:32:48 +0530

    contact horizontal scroll on mobiles

---
+++ src/js/contact.js
@@ -0,0 +1,21 @@
+import flickity from 'flickity';
+
+if (window.innerWidth <= 600) {
+  console.log("asdf");
+  let flkty4 = new flickity(
+    '.carousel-contact', {
+      wrapAround: true,
+      draggable: true,
+      pageDots: false,
+      freeScroll: true
+    }
+  );
+  
+  
+  setTimeout(function () {
+    document.getElementById("contact-container").style.display = "block";
+    flkty4.reloadCells();
+    flkty4.reloadCells();
+    flkty4.next();
+  }, 500);
+}
